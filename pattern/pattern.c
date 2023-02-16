798ab8223a80c010c8c322b1221e75767a7d1aa8
Alireza Abrehforoush <wwe_alireza.abc2015@yahoo.com>
2023-02-16 16:39:06 +0330

    initial commit

---
+++ pattern/pattern.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+int countOccurrences(char * haystack, char * needle)
+{
+    unsigned long long int n = strlen(haystack);
+    unsigned long long int m = strlen(needle);
+    unsigned long long int index = 0;
+    unsigned long long int cnt = 0;
+    strcat(haystack, haystack);
+    while (index < n)
+    {
+        0;
+    }
+}
+
+int main()
+{
+    char x[10] = "aabcdefe";
+    char y[2] = "aa";
+    printf("%s", strstr(x, y));
+    return 0;
+}
